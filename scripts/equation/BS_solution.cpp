ee3e715f82f7edf1c49477067230aee2306352d8
Francisco <francisco.paz.c@gmail.com>
2019-04-28 12:32:50 -0500

    Cambios menores

--- scripts/equation/BS_solution.cpp
+++ scripts/equation/BS_solution.cpp
@@ -4,12 +4,14 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 double b5 = .21;
-/* The following lines are for the derivate approximation */
+
+/*Definition of the function*/
 double fn(double b0,double b1,double b2,double b3,double b4,double x)
 {
 	return b0 + b1*x + b2 *x*x + b3 *x*x*x + b4*x*x*x*x + b5*x*x*x*x*x;
 }
 
+/* The following lines are for the derivate approximation */
 double d1(double b0,double b1,double b2,double b3,double b4,double x0,double h)
 {
     return  (fn(b0,b1,b2,b3,b4,x0 + h) - fn(b0,b1,b2,b3,b4,x0 -h))/(2*h);
@@ -21,19 +23,24 @@ double d2(double b0,double b1,double b2,double b3,double b4,double x0,double h)
 }
 
 double d3(double b0,double b1,double b2,double b3,double b4,double x0,double h){
-	return  ( fn(b0,b1,b2,b3,b4,x0 + 2*h) - fn(b0,b1,b2,b3,b4,x0 -2*h) - 2*( fn(b0,b1,b2,b3,b4,x0 + h) - fn(b0,b1,b2,b3,b4,x0 - h) ) )/ (2*h*h*h);	
+	return  ( fn(b0,b1,b2,b3,b4,x0 + 2*h) - fn(b0,b1,b2,b3,b4,x0 -2*h) - 2*( fn(b0,b1,b2,b3,b4,x0 + h) - fn(b0,b1,b2,b3,b4,x0 - h) ) )/ (2*h*h*h);
 }
 
-/*Evaluar si vale la pena considerar aproximación con error más pequeño*/
+/*The 4th aproximation is not a good one, evaluate if we need a better one for the proyect o(h4 )*/
 double d4(double b0,double b1,double b2,double b3,double b4,double x0,double h){
-	return  ( fn(b0,b1,b2,b3,b4,x0 + 2*h) - 4*fn(b0,b1,b2,b3,b4,x0 + h) + 6*fn(b0,b1,b2,b3,b4,x0) - 4*fn(b0,b1,b2,b3,b4,x0 - h) + fn(b0,b1,b2,b3,b4,x0 - 2*h) )/ (h*h*h*h);	
+	return  ( fn(b0,b1,b2,b3,b4,x0 + 2*h) - 4*fn(b0,b1,b2,b3,b4,x0 + h) + 6*fn(b0,b1,b2,b3,b4,x0) - 4*fn(b0,b1,b2,b3,b4,x0 - h) + fn(b0,b1,b2,b3,b4,x0 - 2*h) )/ (h*h*h*h);
 }
 
+/*Calculate the integrate of the function*/
 
+double in(double b0,double b1,double b2,double b3,double b4,double x0,double t){
+    return 0;
+
+}
 
 int main(int argc, char** argv) {
 	double b0 = 0,b1=0,b2=0,b3=0,b4=0;
-	double x;
+	double x = 0;
 	double result,result2, result3,result4;
 	cout << "dame b0: \n";
 	cin  >> b0;
@@ -51,11 +58,11 @@ int main(int argc, char** argv) {
 	result2 = d2(b0,b1,b2,b3,b4,x,0.001);
 	result3 = d3(b0,b1,b2,b3,b4,x,0.001);
 	result4 = d4(b0,b1,b2,b3,b4,x,0.001);
-	cout << "Resultado d1: " << result << ".\n"; 
-	cout << "Resultado d2: " << result2 << ".\n"; 
-	cout << "Resultado d3: " << result3 << ".\n"; 
-	cout << "Resultado d4: " << result4 << ".\n"; 
-	
-	
+	cout << "Resultado d1: " << result << ".\n";
+	cout << "Resultado d2: " << result2 << ".\n";
+	cout << "Resultado d3: " << result3 << ".\n";
+	cout << "Resultado d4: " << result4 << ".\n";
+
+
 	return 0;
 }
