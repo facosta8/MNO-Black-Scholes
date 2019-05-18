183142ae58149e138a33933defa0ab3e3d3537a2
Francisco Jose Alvarez Rojo <francisco.jose.alvarez.rojo@gmail.com>
2019-05-17 21:52:58 -0500

    grandes avances

--- modelo_simple/regresion.py
+++ modelo_simple/regresion.py
@@ -4,6 +4,8 @@ import pandas as pd
 import datetime
 import numpy as np
 from sklearn.metrics import r2_score
+import sys
+import os
 
 # Extraigo Información de Yahoo
 import fix_yahoo_finance as yf 
@@ -20,10 +22,11 @@ client.cluster
 
 from dask import delayed
 
+os.chdir('git/MNO-Black-Scholes/modelo_simple')
 
 # moment: 'a' = Apertura, 'c'= Cierre, 'h'=máximo, 'l'=mínimo
 @delayed
-def fin_poly_reg(it,investment_length, years_back, future_code, moment, r2_min):
+def fin_poly_reg(investment_length, years_back, future_code, moment, r2_min):
     
     # Tiempo que dura la inversión
     
@@ -36,7 +39,7 @@ def fin_poly_reg(it,investment_length, years_back, future_code, moment, r2_min):
     sys.stdout = open(os.devnull, "w")
     yahoo = yf.download(future_code,b_t,a_t)
     sys.stdout = sys.__stdout__
-    
+        
     
     deg = [1,2,3,4,5,6,7,8,9]
     
@@ -156,16 +159,26 @@ def fin_poly_reg(it,investment_length, years_back, future_code, moment, r2_min):
             if r2 >= r2_min:
                 
                 break
+            
+    """
+    GUARDO LOS RESULTADOS DE YAHOO COMO CSV
+    """
+    X.to_csv(future_code+'.csv')
     
-    if r2 < r2_min:
+    """
+    GUARDO RESULTADOS DE REGRESIÓN EN TXT
         
-        # Probablemente sea más util eliminar esta advertencia
-        print('ADVERTENCIA : r2 no alcanzó el mínimo, 10 bethas por default')
-      
-    nl= '\n'
+
+    sd
+    valor al último día del stock
+    tiempo que dura la inversión
+    betas
     
+    """
+
+
     
-    # Send to txt 
+    nl='\n'
     
         #Betas 
     vec=''   
@@ -174,50 +187,69 @@ def fin_poly_reg(it,investment_length, years_back, future_code, moment, r2_min):
     
         # 
         
-    with open('triplef_'+str(it)+'.txt', 'w') as the_file:
+    with open('data.txt', 'w') as the_file:
         the_file.write(str(str(sd)+nl+str(vu)+nl+str(il)+nl+vec))
         the_file.close()
     
+    """
+    CALCULO RENDIMIENTO CON BLACK-SCHOLES Y LEO RESULTADOS
+    """
     
-    return None
+    os.system('./programa.o > out.txt')
+
+    f = open('out.txt')
+    tocayo = f.readlines()
+    f.close()
     
+    
+    sal = [future_code,X[-1],float(tocayo[0])]
+    
+    rend = 100*(float(sal[1])-float(sal[2]))/float(sal[1])
+    
+    return pd.DataFrame({'commodity':[sal[0]],'last_price':[sal[1]],
+                         'predicted_price':[sal[2]],'difference':rend})
 
-uno = fin_poly_reg(it=1,investment_length=10, years_back=3, future_code="NG=F", moment='a', r2_min=0.5)
+#Pruebas intermedias
+#uno = fin_poly_reg(investment_length=10, years_back=3, future_code="GC", moment='a', r2_min=0.5)
+#uno.compute()
 
-### FORMATO DE DATOS GUARDADOS EN TXT
 
-# sd
-#valor al último día del stock
-#tiempo que dura la inversión
-#betas
 
 
 
 """
-Descargo toda la información de yahoo al mismo tiempo 
+      --------------- JUNTO LA INFORMACIÓN EN UN SÓLO DATAFRAME ---------------
 """
+#lista falsa 
 
-def 
-
-
-
-
-
-"""
-GRAFICA DE COMPROBACIÓN DE REGRESIÓN
+lista = ['1','0.001','CL','CC','KC','CT','LB','LC','NG','SM']
+# 'CL','CC','KC','CT','LB','LC','NG','SM'
 
 
-plot('xlabel', 'ylabel', data=X)
+def rendimientos(lista):
     
-import matplotlib.pyplot as plt
+    to_merge=[]
+    for i in range(len(lista)-2):
+         
+        
+        globals()['data%s'%lista[i+2]] = fin_poly_reg(investment_length=float(lista[0]),
+                                                      years_back=2,future_code=lista[i+2],
+                                                      moment='a', r2_min=float(lista[1]))
+        
+        to_merge.append(globals()['data%s'%lista[i+2]])
+    
+    @delayed
+    def juntar(to_merge):
+        final=pd.concat(to_merge)
+        return final
+    
+    data_f = juntar(to_merge).compute()
+    
+    return data_f
+        
 
-def f(t,z):
-    return z[0]+z[1]*t+z[2]*t**2+z[3]*t**3
+salida = rendimientos(lista)
 
-t1 = np.arange(0.0, 5.0, 0.1)
-t2 = np.arange(0.0, 5.0, 0.02)
+df =pd.concat(salida).concat()
 
-plt.figure(1)
-plt.subplot(211)
-plt.plot(t1, f(t1,z), 'bo', t2, f(t2,z), 'k')
-"""   
+
